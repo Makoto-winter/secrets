7f323d92839bd42143acb93d4bc2652eba75298b
Makoto Teratani <makotot@MakotoMBA.ht.home>
2023-05-27 15:59:42 -0400

    Level4 salting bcrypting complete

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.set('view engine', 'ejs');
 
@@ -38,31 +39,40 @@ app.get("/register", (req, res)=>{
 });
 
 app.post("/register", (req, res)=>{
-  const newUser = new User({
-    email:  req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email:  req.body.username,
+      password: hash
+    });
+
+    newUser.save()
+      .then(()=>{
+        res.render("secrets");
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+
   });
 
-  newUser.save()
-    .then(()=>{
-      res.render("secrets");
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
+
 
 });
 
 app.post("/login", (req, res)=>{
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email:username})
     .then((foundUser)=>{
       if (foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result=== true){
+            res.render("secrets");
+          }
+        });
       }
     })
 })
